2769db8be5ce0cce7cc45d2b887f191e25d39be7
mido <minhkk149@gmail.com>
2018-10-10 10:18:53 +0700

    3rd commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 	def hi
-		render html: "try again push new remote name!"
+		render html: "here is branch!"
 	end
 end
